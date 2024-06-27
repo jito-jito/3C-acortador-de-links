50dc37783631c7cab3107bc6e55d36d815e611d8
sergio sanhueza <sergio@naitus.cl>
2024-06-27 18:47:56 -0400

    chore: add config to .gitignore and clean the app component

---
+++ app/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    fb_apiKey: "",
+    fb_authDomain: "",
+    fb_projectId: "",
+    fb_storageBucket: "",
+    fb_messagingSenderId: "",
+    fb_appId: "",
+    fb_measurementId: ""
+};
