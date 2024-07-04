4729f8b519e9bc2a9f4ffe0d08ba2ec74e3f987f
Daniel Sanchez <danielsanchez1502@gmail.com>
2024-07-04 17:59:27 -0400

    feat: Github integration button

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FirebaseService } from '../shared/libs/firebase.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'app';
+  user: any;
+  isLoading: boolean = false;
+  serviceError: any = false;
 
- 
+  constructor(private _firebaseService: FirebaseService) {}
+
+  async loginGithub() {
+    try {
+      this.isLoading = !this.isLoading;
+      this.user = await this._firebaseService.gitHubSignIn();
+      if (this.user) {
+        this.isLoading = !this.isLoading;
+      }
+    } catch (e) {
+      this.serviceError = true;
+      this.isLoading = !this.isLoading;
+    }
+  }
 }
